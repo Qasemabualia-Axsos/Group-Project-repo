9c35493adb30cb01cc83456ab2e8bc4f4e96497c
farah at <farahabutaha@farahs-MacBook-Pro.local>
2025-10-11 14:28:25 +0300

    actor ad directors templates

--- directors/models.py
+++ directors/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Director(models.Model):
     name = models.CharField(max_length=45)
     bio = models.TextField(blank=True, default="")  # safer: avoids migration errors when DB already has NULLs
+    profile_img = models.ImageField(upload_to="directors/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
